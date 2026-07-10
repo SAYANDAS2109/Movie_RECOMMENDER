d70349d211c1b4b7f0520fff994ebf90f28d2902
Sayan Das <sayan.das21.09.sd@gmail.com>
2026-07-10 15:26:47 +0530

    Complete Movie Recommender System

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pickle as pkl
 
 movies = pkl.load(open(r'C:\C LANGUAGE\code2\Movie_Recommeder\movies.pkl','rb'))
 
-similarity = pkl.load(open(r'C:\C LANGUAGE\code2\Movie_Recommeder\similarity (1).pkl','rb'))
+similarity = pkl.load(open(r'C:\C LANGUAGE\code2\Movie_Recommeder\similarity.pkl','rb'))
 
 st.set_page_config(
     page_title="Movie Recommender",
